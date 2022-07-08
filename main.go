c404d60b2e24a3dcb6372d01156489807739cc7e
yangyu1 <yangyu1@360.cn>
2022-07-08 20:23:29 +0800

    init

--- main.go
+++ main.go
@@ -4,19 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("master")
-	fmt.Println("rebase1")
-	fmt.Println("rebase2")
-	fmt.Println("rebase3")
-	fmt.Println("rebase4")
-	fmt.Println("rebase5")
-	fmt.Println("rebase6")
-	fmt.Println("rebase7")
-	fmt.Println("rebase8")
-	fmt.Println("rebase9")
-	fmt.Println("rebase10")
-	fmt.Println("rebase11")
-	fmt.Println("rebase12")
-	fmt.Println("rebase13")
-	fmt.Println("rebase14")
-	fmt.Println("rebase15")
 }
